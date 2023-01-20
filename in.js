e8ecc7ab75df23eaa12d64f48c24f9ef1a9981cd
sahil ap <sahil ap@gmail.com>
2023-01-20 20:16:17 +0530

    ts

---
+++ in.js
@@ -0,0 +1,6 @@
+function ha() {
+    var message = "hello world";
+    var root = document.createElement('h1');
+    root.textContent = message;
+    document.body.appendChild(root);
+}
---
+++ in.ts
@@ -0,0 +1,6 @@
+function ha(){
+    let message:string="hello world";
+    let root=document.createElement('h1');
+    root.textContent=message;
+    document.body.appendChild(root);
+}
